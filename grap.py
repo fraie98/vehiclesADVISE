10ef3d8313b9806ada64804fd0757d94c91c86ab
yuri <mazzuoli.yuri@gmail.com>
2021-07-16 21:13:09 +0200

    implement plot generation

--- grap.py
+++ grap.py
@@ -1,17 +1,55 @@
+#!python
 import pandas as pd
 import numpy as np
-from scipy.optimize.zeros import results_c
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 file="vehicle_ADVISE_model/Solver/sim/Results_results.csv"
+info = pd.read_csv(file, header=None, error_bad_lines=False, skiprows=32, nrows=4)
+info=np.array(info)
 df = pd.read_csv(file, error_bad_lines=False, skiprows=37)
-
 df.rename(columns={"Experiment 1":"Experiment"}, inplace=True)
-df=df[["Experiment","Name", "Start Time", "Mean", "Confidence Interval"]]
+df=df[["Experiment","Result Type","Name", "Start Time", "Mean", "Confidence Interval"]]
 
 ex=df.groupby(["Experiment"], as_index=False)["Experiment"].first()
 ex=np.array(ex["Experiment"])
-
+cmap=cm.get_cmap("rainbow")
+goals=["DoS", "vehicleUndesideredBehaviour", "dataBreach"]
+p_colors=cmap(np.linspace(0, 1 ,20))
 for t in ex:
-    if(t[0]=='E'):
-        data=df[df["Experiment"]==t]
-        data=data[data["Name"]=="DoS"]
-        print(data[["Start Time", "Mean",  "Confidence Interval"]])
+    if(t[0]!='E'):
+        continue
+    data=df[df["Experiment"]==t]
+    if(info==[]):
+        info=np.array( np.empty((4,4)), np.chararray)
+        inf=data[["Experiment","Result Type","Name", "Start Time"]]
+        info[0]=np.array(inf[inf["Name"]=="IDSsensitivity"])
+        info[1]=np.array(inf[inf["Name"]=="attacker"])
+        info[2]=np.array(inf[inf["Name"]=="codeObfuscation"])
+        info[3]=np.array(inf[inf["Name"]=="firewallSensitivity"])
+    if(info[1,3]=="'H'"):
+        plot=1
+        attacker="Hacker"
+    if(info[1,3]=="'P'"):
+        plot=2
+        attacker="Physical Intruder"
+    if(info[1,3]=="'I'"):
+        attacker="Insider"
+        plot=3
+    for i in range(0,3):
+        plt.figure(plot+i*3)
+        prob=data[data["Name"]==goals[i]]
+        prob=np.array(prob[["Start Time", "Mean",  "Confidence Interval"]]).astype(float)
+        plt.title(attacker+" "+goals[i])
+        plt.xlabel("time")
+        plt.xticks(np.arange(0,220,20))
+        plt.ylabel("probability")
+        #plt.yticks(np.arange(0,1.1,11))
+        label=str(info[:,2:])
+        plt.errorbar(x=prob[:,0], y=prob[:,1], yerr=prob[:,2], capsize=3, linestyle="solid",
+               marker='s', markersize=3, mfc="black", mec="black", label=label)
+        plt.legend(title="Experiments", loc='lower center', bbox_to_anchor=(1,0.5))
+        print(t)
+        print(info)
+    info=[]
+
+plt.show()
